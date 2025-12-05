4d3c416736e6085e2ae45b72e479b06415cf6bf3
SoTBeR <Vasyustenko@gmail.com>
2025-12-05 12:11:44 +0300

    text_files.py

--- utils/validators.py
+++ utils/validators.py
@@ -3,6 +3,18 @@ from enum import Enum, StrEnum
 
 
 def num_validator(value: int, min_value: int, max_value: int):
+    """
+    Валидатор целых чисел.
+
+    Args:
+        value: Целое число для валидации
+        min_value: Минимальное значение
+        max_value: Максимальное значение
+
+    Returns:
+        Целое число после валидации.
+    """
+
     if min_value <= value <= max_value:
         return value
     else:
@@ -13,4 +25,15 @@ def num_validator(value: int, min_value: int, max_value: int):
 
 
 def enum_value_validator(value: str, enum_class: type[Union[Enum, StrEnum]]) -> bool:
+    """
+    Валидатор типов данных Enum и StrEnum.
+
+    Args:
+        value: Значение для валидации
+        enum_class: Класс Enum или StrEnum
+
+    Returns:
+        Соответствует ли значение классу
+    """
+
     return any(value.lower() == member.value for member in enum_class)
