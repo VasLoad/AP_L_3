c7f71bdd88e681964d35bf623ab118412642c82d
SoTBeR <Vasyustenko@gmail.com>
2025-12-05 10:59:43 +0300

    import re from typing import Optional
    from errors import FileSuffixError, FileReadError
    
    from utils.storage import load_txt
    
    def load_text_from_file_with_regex(path: str, regex_pattern: Optional[str] = None) -> list[str]:
        "

--- utils/text_files.py
+++ utils/text_files.py
@@ -1,22 +1,28 @@
 import re
 from typing import Optional
 
+from errors import FileSuffixError, FileReadError
+
 from utils.storage import load_txt
 
 
-def load_text_from_file(path: str) -> str:
-    try:
-        text = load_txt(path)
-    except Exception:
-        raise
-
-    return text
-
-
 def load_text_from_file_with_regex(path: str, regex_pattern: Optional[str] = None) -> list[str]:
+    """
+    Получает данные из файла прогоняет их через регулярное выражение.
+
+    Args:
+        path: Путь к файлу
+        regex_pattern: Паттерн регулярного выражения (опционально)
+
+    Raises:
+        FileSuffixError: Неверное расширение файла данных
+        FileReadError: Ошибка при чтении файла данных
+    """
     try:
         text = load_txt(path)
-    except Exception:
+    except FileSuffixError:
+        raise
+    except FileReadError:
         raise
 
     return re.findall(re.compile(regex_pattern), text)
